8e010441b4313fc37f9e9e83c6aea812354a26c0
Panda00007 <35125688+Panda00007@users.noreply.github.com>
2019-09-30 09:36:31 +0530

    Getting logged user id from session [Bug Fixed]

--- pay.php
+++ pay.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require '../iwt/Admin/controllers/db.php';
 // Check connection
 
@@ -11,10 +12,11 @@ $c_no = $_POST["cardnumber"];
 $e_date = $_POST["paydate"];
 $s_code = $_POST["securitycode"];
 $b_add = $_POST["Billingaddress"];
+$u_id = $_SESSION["logged_user_id"];
 
 
 //attempt insert query execution
-$sql = "INSERT INTO pay (pay_id,user_id,card_type,fname,card_no,expire_date,sec_code,bill_address) VALUE ('','2','$pay_pr','$c_type','$f_name','$c_no','$e_date','$s_code','$b_add')";
+$sql = "INSERT INTO pay (user_id,card_type,fname,card_no,expire_date,sec_code,bill_address) VALUE ('$u_id','$pay_pr','$c_type','$f_name','$c_no','$e_date','$s_code','$b_add')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
